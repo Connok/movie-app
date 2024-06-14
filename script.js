642d405283bc5588e68e2ab8f7850705b5d7fbc1
Connor McCarthy <mccarthyrcconnor@gmail.com>
2024-06-13 22:58:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const APILINK = 'https://api.themoviedb.org/3/movie/550?api_key=bbe9a176874705f99a4ce9047e173b11&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie&api_key=bbe9a176874705f99a4ce9047e173b11&query=";
+
+
